020e2be1b929d451ee9f2f115512bba361f13c07
lea <lea.herrera.7e7@itb.cat>
2024-01-17 12:16:09 +0100

    gg

--- app/src/main/java/com/example/trivial/model/GameStructure.kt
+++ app/src/main/java/com/example/trivial/model/GameStructure.kt
@@ -1,9 +1,16 @@
 
 data class Preguntas(
     val dificultad: dificultad ,
-    val pre
+    val enunciado: String,
+    val respuesta1:String,
+    val respuesta2: String,
+    val respuesta3: String,
+    val resultCorrect:Int
 )
 
 enum class dificultad {
     facil , normal , dificil , experto
+}
+enum class categoria {
+    geografia , Historia , entretenimiento , ciencias , deportes
 }
