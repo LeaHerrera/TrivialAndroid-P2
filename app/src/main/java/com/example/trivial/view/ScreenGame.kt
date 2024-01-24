45e8978c2b0c501f345c910d2054aaf16a9be1c8
lea <lea.herrera.7e7@itb.cat>
2024-01-24 10:03:18 +0100

    gg

--- app/src/main/java/com/example/trivial/view/ScreenGame.kt
+++ app/src/main/java/com/example/trivial/view/ScreenGame.kt
@@ -1,5 +1,4 @@
 import android.os.CountDownTimer
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -9,9 +8,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -21,56 +18,66 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.trivial.model.Dificultad
 import com.example.trivial.model.Preguntas
+import com.example.trivial.navigation.Routes
 
 
 @Composable
 fun ScreenGame(navController: NavController, myViewModel: MyViewModel) {
 
-    val tiempoInicialMilisegundos = ( myViewModel.ajustes.segundos*1000 ).toLong() // 5 segundos
-    val intervaloMilisegundos = 1000L // 1 segundo
-    var tiempo:String by rememberSaveable { mutableStateOf("") }
 
-    
 
     Column (
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
 
     ){
+        val tiempoInicialMilisegundos = ( myViewModel.ajustes.segundos*1000 ).toLong() // 5 segundos
+        val intervaloMilisegundos = 1000L // 1 segundo
+        var tiempo:String by rememberSaveable { mutableStateOf("") }
 
-        var questionIndice by rememberSaveable { mutableStateOf( 0 ) }
-        var question = myViewModel.preguntas[questionIndice]
+
+        var question = myViewModel.preguntas[myViewModel.indicePregunta]
 
         val cuentaAtras = object : CountDownTimer(tiempoInicialMilisegundos, intervaloMilisegundos) {
             override fun onTick(millisUntilFinished: Long) {
                 // Se llama cada vez que el contador cuenta un intervalo (cada segundo en este caso)
                 val segundosRestantes = millisUntilFinished / 1000
                 tiempo = "Segundos restantes: $segundosRestantes"
+                myViewModel.MantenerReloj()
             }
 
             override fun onFinish() {
                 // Se llama cuando la cuenta atrás termina
-                tiempo = "¡Cuenta atrás terminada!"
+                tiempo = "¡Se acabo el tiempo!"
                 myViewModel.quitarCount()
+
+                if (myViewModel.contador != 0){
+                    myViewModel.añadirReloj()
+                    myViewModel.randowPregunta()
+                }
             }
         }
 
-        if (myViewModel.contador % 2 == 0){
-            questionIndice = myViewModel.randowPregunta()
-            myViewModel.quitarCount()
-            cuentaAtras.start()
-        } else {
-            Text(text = tiempo)
-            playQuestions( question, myViewModel )
+        if (myViewModel.contador == 0){
+            navController.navigate(Routes.PantallaFinal.route)
         }
+
+        if (myViewModel.reloj){
+            cuentaAtras.cancel()
+            cuentaAtras.start()
+        }
+
+        Text(text = tiempo)
+        Text(text = " ${myViewModel.reloj}")
+        playQuestions( question, myViewModel )
 
     }
 }
 
+
 @Composable
-fun playQuestions(question: Preguntas, myViewModel: MyViewModel){
+fun playQuestions(question: Preguntas, myViewModel: MyViewModel) {
 
     Text(text = question.enunciado , fontSize = 20.sp , fontWeight = FontWeight.Bold )
     botones(question, myViewModel )
@@ -78,84 +85,97 @@ fun playQuestions(question: Preguntas, myViewModel: MyViewModel){
 }
 
 @Composable
-fun botones(question: Preguntas, myViewModel: MyViewModel){
+fun botones(question: Preguntas, myViewModel: MyViewModel) {
 
-    var color1:Color by remember { mutableStateOf( Color.Gray ) }
-    var color2:Color by remember { mutableStateOf( Color.Gray ) }
-    var color3:Color by remember { mutableStateOf( Color.Gray ) }
-    var color4:Color by remember { mutableStateOf( Color.Gray ) }
+    var color1: Color = (Color.Gray)
+    var color2: Color = (Color.Gray)
+    var color3: Color = (Color.Gray)
+    var color4: Color = (Color.Gray)
 
     Button(
         onClick = {
-            if (question.resultCorrect == question.respuesta1){
+            if (question.resultCorrect == question.respuesta1) {
                 color1 = Color.Green
                 myViewModel.añadirResCorrecta()
             } else {
                 color1 = Color.Red
             }
             myViewModel.quitarCount()
+            myViewModel.randowPregunta()
+            myViewModel.añadirReloj()
         },
         Modifier
             .width(300.dp)
             .padding(10.dp), //margen
         colors = ButtonDefaults.buttonColors(color1)
-    ){
-        Text(text = question.respuesta1 , fontSize = 25.sp , fontWeight = FontWeight.Bold )
+    ) {
+        Text(text = question.respuesta1, fontSize = 25.sp, fontWeight = FontWeight.Bold)
     }
 
     Button(
         onClick = {
-            if (question.resultCorrect == question.respuesta2){
+            if (question.resultCorrect == question.respuesta2) {
                 color2 = Color.Green
                 myViewModel.añadirResCorrecta()
             } else {
                 color2 = Color.Red
             }
             myViewModel.quitarCount()
+            myViewModel.randowPregunta()
+            myViewModel.añadirReloj()
+
         },
         Modifier
             .width(300.dp)
             .padding(10.dp), //margen
-    colors = ButtonDefaults.buttonColors(color2)
-    ){
-        Text(text = question.respuesta2 , fontSize = 25.sp , fontWeight = FontWeight.Bold )
+        colors = ButtonDefaults.buttonColors(color2)
+    ) {
+        Text(text = question.respuesta2, fontSize = 25.sp, fontWeight = FontWeight.Bold)
     }
 
     Button(
         onClick = {
-            if (question.resultCorrect == question.respuesta3){
+            if (question.resultCorrect == question.respuesta3) {
                 color3 = Color.Green
                 myViewModel.añadirResCorrecta()
             } else {
                 color3 = Color.Red
             }
             myViewModel.quitarCount()
+            myViewModel.randowPregunta()
+            myViewModel.añadirReloj()
+
         },
         Modifier
             .width(300.dp)
             .padding(10.dp), //margen
         colors = ButtonDefaults.buttonColors(color3)
-    ){
-        Text(text = question.respuesta3 , fontSize = 25.sp , fontWeight = FontWeight.Bold )
+    ) {
+        Text(text = question.respuesta3, fontSize = 25.sp, fontWeight = FontWeight.Bold)
     }
 
     Button(
         onClick = {
-            if (question.resultCorrect == question.respuesta4){
+            if (question.resultCorrect == question.respuesta4) {
                 color4 = Color.Green
                 myViewModel.añadirResCorrecta()
             } else {
                 color4 = Color.Red
             }
             myViewModel.quitarCount()
+            myViewModel.randowPregunta()
+            myViewModel.añadirReloj()
+
+
         },
         Modifier
             .width(300.dp)
             .padding(10.dp), //margen
         colors = ButtonDefaults.buttonColors(color4)
-    ){
-        Text(text = question.respuesta4 , fontSize = 25.sp , fontWeight = FontWeight.Bold )
+    ) {
+        Text(text = question.respuesta4, fontSize = 25.sp, fontWeight = FontWeight.Bold)
     }
+
 }
 
 
--- app/src/main/java/com/example/trivial/viewModel/variables.kt
+++ app/src/main/java/com/example/trivial/viewModel/variables.kt
@@ -1,3 +1,7 @@
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.trivial.model.Dificultad
 import com.example.trivial.model.Preguntas
@@ -6,43 +10,53 @@ import com.example.trivial.model.Settings
 class  MyViewModel:ViewModel () {
 
     var preguntas:Array<Preguntas> = arrayOf(
-        Preguntas(Dificultad.facil,"FACIL","A","B","C" ,"D","C"),
-        Preguntas(Dificultad.normal,"normal","A","B","C" ,"D","B"),
-        Preguntas(Dificultad.dificil,"dificil","A","B","C" ,"D","A"),
-        Preguntas(Dificultad.experto,"experto","A","B","C" ,"D","D")
+        Preguntas(Dificultad.facil,"FACIL1","A","B","C" ,"D","C"),
+        Preguntas(Dificultad.facil,"FACIL2","A","B","C" ,"D","D"),
+        Preguntas(Dificultad.facil,"normal","A","B","C" ,"D","B"),
+        Preguntas(Dificultad.facil,"dificil","A","B","C" ,"D","A"),
+        Preguntas(Dificultad.facil,"experto","A","B","C" ,"D","D")
 
     )
         private set
 
     var ajustes:Settings = Settings(Dificultad.facil , 5 , 10)
         private set
-    var respuestasCorrectas = 0
+    var respuestasCorrectas by mutableStateOf(0)
         private set
 
-     var contador = ajustes.rondas * 2
+    var contador by mutableStateOf(ajustes.rondas)
         private set
+
+    var indicePregunta by mutableStateOf( 0)
+        private set
+
+    var reloj by mutableStateOf( true)
+    private set
 
     fun añadirResCorrecta(){
         respuestasCorrectas++
     }
 
     // OBTENER PREGUNTA ALEATORIA
-    fun randowPregunta():Int{
+    fun randowPregunta(){
 
         val pregunta:Int
 
         var cadena:Array<Preguntas> = preguntaNivel( ajustes.dificultad , preguntas )
         pregunta = preguntaAleatoria(cadena)
 
-        return pregunta
+        indicePregunta = pregunta
 
     }
 
-    fun añadirCount(){
-        contador++
+    fun añadirReloj(){
+        reloj = true
     }
+    fun MantenerReloj(){
+        reloj = false
+     }
     fun quitarCount(){
-        contador++
+        contador--
     }
     private fun preguntaNivel(difficulty:Dificultad, cadena:Array<Preguntas>):Array<Preguntas>{
 
